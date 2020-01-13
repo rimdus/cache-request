9d6766028e503f9e3bfb65b9a29dfe98c6fdde31
rimdus <rimdus@yandex.ru>
2020-01-13 18:26:51 +0700

    fix cache tests

--- tests/cache.test.ts
+++ tests/cache.test.ts
@@ -1,4 +1,5 @@
 import { Cache } from '../lib/component/cache';
+import { assert } from 'chai';
 
 let cache: Cache;
 
@@ -27,14 +28,41 @@ describe('cache', () => {
     }
     if (rep) console.log('Repeats:', rep);
     cache.off();
-    return rep === 0;
+    assert.equal(rep, 0);
   });
 
-  it('check mutate after clone', () => {
+  it('check clone', () => {
     const obj = { a: { b: { c: { text: 123 } } } };
     cache.add({ action: 'clone' }, obj);
     obj.a.b.c.text = 456;
     const res = cache.get({ action: 'clone' });
-    return res.a.b.c.text !== obj.a.b.c.text;
+    assert.notEqual(res.a.b.c.text, obj.a.b.c.text);
+  });
+
+  it('string key', () => {
+    const obj = { a: 123 };
+    cache.add('customstringkey', obj);
+    const res = cache.get('customstringkey');
+    assert.equal(res.a, obj.a);
+  });
+
+  it('custom conv key function', () => {
+    const cache = new Cache({ keyConvertFn: (unique: any) => unique.uuid });
+    const obj = { uuid: '123456789', data: { a: 8 } };
+    cache.add(obj, obj);
+    const res = cache.get(obj);
+    cache.off();
+    assert.equal(res.data.a, obj.data.a);
+  });
+
+  it('count', () => {
+    const cache = new Cache();
+    const obj = { a: 0 };
+    for (let i = 0; i < 100; i++) {
+      cache.add(obj, obj);
+      obj.a += 1;
+    }
+    cache.off();
+    assert.equal(cache.count(), 100);
   });
 });
